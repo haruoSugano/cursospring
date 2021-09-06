ad9914e7bbe8494976313225a6126f5f4677e58a
Haruo Sugano <haruosugano@icloud.com>
2021-09-05 21:25:55 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/haruo/cursospring/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.haruo.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.haruo.cursospring.domain.Categoria;
+import com.haruo.cursospring.domain.Cliente;
+import com.haruo.cursospring.repositories.ClienteRepository;
+import com.haruo.cursospring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName() ));
+	}
+}
